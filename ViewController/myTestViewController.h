c79468480d1f1b7eb49ac64c129ebd0ae9d7f0b0
丁嘉睿 <857133319@qq.com>
2017-12-11 11:32:05 +0800

    -测试

---
+++ ViewController/myTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  myTestViewController.h
+//  JRSystemActionSheetView
+//
+//  Created by Mr.D on 2017/12/11.
+//  Copyright © 2017年 Mr.D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myTestViewController : UIViewController
+
+@end
---
+++ ViewController/myTestViewController.m
@@ -0,0 +1,46 @@
+//
+//  myTestViewController.m
+//  JRSystemActionSheetView
+//
+//  Created by Mr.D on 2017/12/11.
+//  Copyright © 2017年 Mr.D. All rights reserved.
+//
+
+#import "myTestViewController.h"
+
+@interface myTestViewController ()
+
+
+@end
+
+@implementation myTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (void)dealloc{
+
+}
+
+- (void)bbq{
+
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
